2387fdbf4095f092be12407b806e6cca0f923c01
KananTapdigli <kananet@code.edu.az>
2020-05-14 20:04:29 +0400

    Completed

---
+++ Invoice/Invoice/Invoice.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Invoice
+{
+    class Invoice
+    {
+
+        private string _provider;
+        private string _account;
+        private string _customer;
+
+        public Invoice(string account, string customer, string provider)
+        {
+            this._provider = provider;
+            this._account = account;
+            this._customer = customer;
+        }
+
+        public string article { get; set; }
+        public int quantity { get; set; }
+        public double cost { get; set; }
+
+        public void CostCalculation(bool needEdv)
+        {
+            if (needEdv == true)
+            {
+                Console.WriteLine($"Mehsulun adi:{article}, mehsulun qiymeti: {cost}, mesulun sayi: {quantity}, umumi mebleg(EDV odenilir):{cost * quantity + cost * 0.18}");
+            }
+            else
+            {
+                Console.WriteLine($"Mehsulun adi:{article}, mehsulun qiymeti: {cost}, mesulun sayi: {quantity}, umumi mebleg(EDV odenilmir):{cost * quantity}");
+            }
+        }
+    }
+}
---
+++ Invoice/Invoice/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Invoice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Invoice obj = new Invoice("678904", "Alex", "Foxtrot");
+            obj.article = "laptop";
+            obj.quantity = 6;
+            obj.cost = 1400;
+            obj.CostCalculation(true);
+        }
+    }
+}
